09e04e98adda14c710ddd460ac5a78ba5cd086d1
Parth <3916189+parthsagdeo@users.noreply.github.com>
2020-01-01 17:46:05 -0500

    Revised certain simulation code to lead to better graphs.

--- controller.py
+++ controller.py
@@ -19,10 +19,12 @@ def get_color_at_datetime(dt):
 
 
 def rgbw_to_rgb_brightness(rgbw_tuple):
-    brightness = (rgbw_tuple[0] + rgbw_tuple[1] + rgbw_tuple[2] + rgbw_tuple[3]) / 400.0
+    brightness = (rgbw_tuple[0] + rgbw_tuple[1] + rgbw_tuple[2] + rgbw_tuple[3]) / 4.0
 
-    rgb_tuple_0_200 = (rgbw_tuple[0] + rgbw_tuple[3]), (rgbw_tuple[1] + rgbw_tuple[3]), (rgbw_tuple[2] + rgbw_tuple[3])
-    rgb_tuple_0_255 = numpy.multiply(rgb_tuple_0_200, 255.0/200)
+    max_color_val = max(rgbw_tuple[0:3])
+    color_multiplier = 100/max_color_val
+
+    rgb_tuple_0_255 = numpy.multiply(rgbw_tuple[0:3], 2.54 * color_multiplier)
 
     return rgb_tuple_0_255, brightness
 
@@ -30,18 +32,27 @@ def rgbw_to_rgb_brightness(rgbw_tuple):
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
 
-    start_datetime = datetime.datetime(2019, 12, 29, 23, 0, 0)
+    start_datetime = datetime.datetime(2019, 12, 30, 0, 1, 0)
     end_datetime = datetime.datetime(2019, 12, 30, 1, 0, 0)
 
     step_size = (end_datetime - start_datetime).total_seconds() / 100
+    x_vals = numpy.arange(start_datetime.timestamp(), end_datetime.timestamp(), step_size)
 
-    for i in numpy.arange(start_datetime.timestamp(), end_datetime.timestamp(), step_size):
+    blue_magnitude = []
+
+    for i in x_vals:
         dt = datetime.datetime.fromtimestamp(i)
         rgbw = get_color_at_datetime(dt)
         color_rgb, brightness = rgbw_to_rgb_brightness(rgbw)
 
+        blue_magnitude.append((rgbw[2] + rgbw[3]/3.0)/4.0)  # blue light
+
         color = list(map(lambda div: div / 255.0, color_rgb)) + [1]
         print(color)
-        plt.plot((i, i), (0, brightness/100), linewidth=step_size / 2.0, linestyle="-", color=color)
+        plt.bar([i], [brightness], width=step_size, color=color)
 
+    plt.plot(x_vals, blue_magnitude)
+
+    plt.xticks(x_vals[::8],
+               list(map(lambda x: datetime.datetime.fromtimestamp(x).strftime("%H:%M"), x_vals[::8])))
     plt.show()
